67edbfcb022eb53c6ffe8a78ba605d4bf606920b
Marco Sciacovelli <36106337+Marcosciaco@users.noreply.github.com>
2022-11-12 13:14:53 +0100

    Added the Rest API to get the values from the sensors

---
+++ localBackend/vyousense/views.py
@@ -0,0 +1,43 @@
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.cache import never_cache
+from .sensors import Distance, Temperature
+
+import datetime
+
+last_person_when = None
+people_counter = 0
+
+@never_cache
+def temperature(request):
+	t = 0.0
+	try:
+		t = Temperature.check_temperature()
+	except:
+		return JsonResponse({"status": "ko"})
+	return JsonResponse({"status": "ok", "data": {"temperature": t}})
+
+@never_cache
+def proximity(request):
+	p = 0.0
+	try:
+		p = Distance.check_distance()
+	except:
+		return JsonResponse({"status": "ko"})
+	return JsonResponse({"status": "ok", "data": {"distance": p}})
+
+@csrf_exempt
+def person_detected(request):
+	global people_counter, last_person_when
+	if request.method != "POST":
+		return JsonResponse({"status": "ko"})
+	people_counter += 1
+	now = datetime.date.today()
+	if last_person_when == None or last_person_when != now:
+		people_counter = 1
+		last_person_when = now
+	return HttpResponse("ok", status=200)
+
+@never_cache
+def people_count(request):
+	return JsonResponse({"status": "ok", "data": {"count": people_counter}})
