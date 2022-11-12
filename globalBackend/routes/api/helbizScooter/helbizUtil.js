be47c25e04ef72ea2433fb2c94f798171eaaf386
Elias Binder <ebinder@unibz.it>
2022-11-12 03:26:17 +0100

    Make backend to auto update the scooters

--- globalBackend/routes/api/helbizScooter/helbizUtil.js
+++ globalBackend/routes/api/helbizScooter/helbizUtil.js
@@ -3,24 +3,30 @@ const authConfig = require('./auth.json');
 let token = null;
 
 module.exports.init = function() {
-    fetch('https://api.helbiz.com/prod/user/authenticate', {
-        method: 'POST',
-        headers: {
-            'User-Agent': 'Helbiz (com.helbiz.android)',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            email: authConfig.email,
-            password: authConfig.password
-        })
-    }).then(res => res.json())
-        .then(json => token = json.token)
-        .then(() => console.log('Initialized helbizUtil'));
+    const updateFunc = async function () {
+        fetch('https://api.helbiz.com/prod/user/authenticate', {
+            method: 'POST',
+            headers: {
+                'User-Agent': 'Helbiz (com.helbiz.android)',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email: authConfig.email,
+                password: authConfig.password
+            })
+        }).then(res => res.json())
+            .then(json => token = json.token)
+            .then(() => console.log('Updated helbiz information'));
+    }();
+    setInterval(async function () {
+        updateFunc();
+    }, 60 * 10 * 1000);
+
 }
 
 module.exports.getScooters = async function(lat, long) {
-    const northWest = (lat-0.05)+'%2C'+(long+0.05);//(lat)+'%2C'+long; //FIXME: does not work for greenwich
-    const southEast = (lat+0.005)+'%2C'+(long-0.005);//(lat-0.01)+'%2C'+(long-0.01); //FIXME: does not work for greenwich
+    const northWest = (lat-0.01)+'%2C'+(long+0.01); //FIXME: does not work for greenwich
+    const southEast = (lat+0.01)+'%2C'+(long-0.01); //FIXME: does not work for greenwich
     let resp = await fetch('https://api.helbiz.com/prod/scooters?northWest=' + northWest + '&southEast=' + southEast, {
         method: 'GET',
         headers: {
