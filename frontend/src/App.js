b2fd53b69e21ec791f9ddd4f4fec3812f715957a
Francesco <francesco.mazzini432@gmail.com>
2022-11-12 05:56:48 +0100

    Added routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Homepage from './components/homepage';
 import HomepageOn from './components/homepageOn';
 import ScootersPage from './components/scootersPage';
 import BikesPage from './components/bikesPage';
+import { BrowserRouter as Router, Switch, 
+  Route, Redirect,} from "react-router-dom";
 import './App.css';
 
 function App() {
