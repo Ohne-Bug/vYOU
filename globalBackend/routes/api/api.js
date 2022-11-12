3ac49db4c05d8320a8b9620a64c3afc4fa22d2fa
Elias Binder <ebinder@unibz.it>
2022-11-12 02:44:08 +0100

    Global Backend - Implement quiz stuff

--- globalBackend/routes/api/api.js
+++ globalBackend/routes/api/api.js
@@ -4,10 +4,12 @@ var router = express.Router();
 const ecoTipsRouter = require('./ecoTips/router');
 const helbizScooterRouter = require('./helbizScooter/router');
 const bikeMiRouter = require('./bikeMi/router');
+const quizRouter = require('./quiz/router');
 
 router.use('/ecotips', ecoTipsRouter);
 router.use('/helbizscooter', helbizScooterRouter);
 router.use('/bikemi', bikeMiRouter);
+router.use('/quiz', quizRouter);
 
 router.get('/', function(req, res, next) {
     res.json({
--- globalBackend/routes/api/quiz/router.js
+++ globalBackend/routes/api/quiz/router.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+const quizData = require('./quizData');
 
 router.get('/get', function(req, res, next) {
-    const lat = req.query.lat;
-    const long = req.query.long;
-    if (lat && long) {
-        helbizUtil.getScooters(lat, long).then((data) => {
-            res.json({
-                "helbizScooter": data
-            })
-        });
-    } else{
-        res.json({
-            "status": "error",
-            "message": "lat and long are required"
-        })
+    let quiz = {...quizData}.questions;
+    for (let i of quiz) {
+        i.correctAnswer = undefined;
     }
+    res.json(quiz);
+});
+
+router.post('/check', function(req, res, next) {
+    let quiz = quizData.questions;
+    let score = 0;
+    for (let i of quiz) {
+        if (i.correctAnswer === req.body[i.id]) {
+            score++;
+        }
+    }
+    if (score === quiz.length) {
+
+    }
+    res.json({
+        score
+    });
 });
 
 module.exports = router;
